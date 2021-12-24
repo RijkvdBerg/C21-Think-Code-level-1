a9f34eaf67176a2028442cf8b8a926e1a8974a88
RijkvdBerg <2146514@talnet.nl>
2021-12-24 13:32:27 +0100

    opdracht 5 gemaakt

---
+++ Opdracht5/opdracht 05.py
@@ -0,0 +1,8 @@
+# Mijn favoriete kleuren
+
+def favoriete_kleur(name):
+    print("Een van mijn favoriete kleuren is", name)
+
+favoriete_kleur("rood")
+favoriete_kleur("blauw")
+favoriete_kleur("paars")
